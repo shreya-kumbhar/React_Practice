35fa9860479b0a12eab90f2cfb72ff2410bd08db
shreya-kumbhar <shreyakumbhar9829@gmail.com>
2025-01-29 11:46:28 +0530

    create first react app.

---
+++ recatset1/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Sample from "./component/Sample";
+
+function App() {
+  return (
+    <div className="App">
+
+      {/*Sample function*/}
+      <Sample></Sample>
+
+    </div>
+  );
+}
+
+export default App;
