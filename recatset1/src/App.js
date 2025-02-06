8a430751f3b4cd0559e7b50d52a599817505ef06
shreya-kumbhar <shreyakumbhar9829@gmail.com>
2025-02-06 12:40:32 +0530

    created props pass in react js

--- recatset1/src/App.js
+++ recatset1/src/App.js
@@ -1,24 +1,16 @@
 
 // import Chai from "./component/Chai";
 // import TeaBreak from "./component/Chai";
-
+// import Multiline from "./component/Multiline";
 import React from "react";
-import Multiline from "./component/Multiline";
+import PropsDisplayer from "./component/Propspass";
 
 function App()
 {
     return (
-        <blockquote>
-            <p>
-                Hello we are learning react js in codecademy !!!
-            </p>
-            <cite>
-                <a target="_blank"
-                   href="https://en.wikipedia.org/wiki/Douglas_Huebler">
-                    Douglas Huebler
-                </a>
-            </cite>
-        </blockquote>
+       <>
+        < PropsDisplayer></PropsDisplayer>
+       </>
     );
 }
 export default App;
